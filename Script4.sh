f9684e59edd1d8b663b122acf498f63a981947cf
OnkarKadam1 <ok.kadam7@gmail.com>
2024-11-22 00:13:49 +0530

    Adding 2 new sript files of env path and shell variable

---
+++ Script4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is creation and calling of shell variable --- defining variables
+Name=Onkar-Kadam
+Age=26
+PROFESSION=AWS/DevSecOps/Azure DevOps
+echo "My name is $Name, my age $Age, I am learning $PROFESSION"
